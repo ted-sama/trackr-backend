93a8790d28ec05f9b2655be75abff94b5f2bb456
Teddy <teddynsoki@gmail.com>
2025-10-03 20:31:46 +0200

    Update books table migration to change title column type from string to text for improved flexibility in storing book titles.

--- database/migrations/1750359319188_create_books_table.ts
+++ database/migrations/1750359319188_create_books_table.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title').notNullable()
+      table.text('title').notNullable()
       table.string('cover_image').nullable()
       table.string('type').nullable()
       table.decimal('rating', 3, 2).nullable()
